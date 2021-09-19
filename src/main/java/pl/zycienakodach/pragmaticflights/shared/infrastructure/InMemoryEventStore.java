e8cea22081ba3cb516ee7ad083e297e27867a473
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-09-19 19:55:28 +0200

    Finish InMemoryEventStore implementation

--- src/main/java/pl/zycienakodach/pragmaticflights/shared/infrastructure/InMemoryEventStore.java
+++ src/main/java/pl/zycienakodach/pragmaticflights/shared/infrastructure/InMemoryEventStore.java
@@ -12,13 +12,14 @@ import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 import static java.util.Collections.emptyList;
 
 class InMemoryEventStore implements EventStore, EventSource {
 
   private final EventBus eventBus;
-  private ConcurrentMap<EventStreamName, List<DomainEvent>> streams = new ConcurrentHashMap<>();
+  private final ConcurrentMap<EventStreamName, List<DomainEvent>> streams = new ConcurrentHashMap<>();
 
   InMemoryEventStore(EventBus eventBus) {
     this.eventBus = eventBus;
@@ -39,16 +40,27 @@ class InMemoryEventStore implements EventStore, EventSource {
   public void write(EventStreamName eventStreamName, List<DomainEvent> events, ExpectedStreamVersion expectedStreamVersion) {
     streams.compute(eventStreamName, (__, streamEvents) -> {
       var currentStreamVersion = new EventStream(streamEvents).version();
-      var isExpectedVersion = switch (expectedStreamVersion) {
-        case ExpectedStreamVersion.NotExists ignored && currentStreamVersion == 0 -> true;
-        case ExpectedStreamVersion.Exactly v && currentStreamVersion == v.raw() -> false;
-        case ExpectedStreamVersion.Any ignored -> true;
-        default -> false;
-      };
-      if(!isExpectedVersion){
-        throw new IllegalStateException("Expected event stream version is $expectedStreamVersion but current is $currentStreamVersion!")
-      }
-      return streamEvents;
+      eventStreamWriteOptimisticLocking(expectedStreamVersion, currentStreamVersion);
+      return streamEvents == null ? List.copyOf(events) : Stream.concat(streamEvents.stream(), events.stream()).toList();
     });
-  };
+    eventBus.publishAll(events);
+  }
+
+  private void eventStreamWriteOptimisticLocking(ExpectedStreamVersion expectedStreamVersion, int currentStreamVersion) {
+    var isExpectedVersion = switch (expectedStreamVersion) {
+      case ExpectedStreamVersion.NotExists ignored:
+        yield currentStreamVersion == 0;
+      case ExpectedStreamVersion.Exactly v:
+        yield currentStreamVersion == v.raw();
+      case ExpectedStreamVersion.Any ignored:
+        yield true;
+      default:
+        throw new IllegalStateException("Unexpected value: " + expectedStreamVersion);
+    };
+    if(!isExpectedVersion){
+      throw new IllegalStateException("Expected event stream version is " + expectedStreamVersion + " but current is " + currentStreamVersion);
+    }
+  }
+
+  ;
 }
