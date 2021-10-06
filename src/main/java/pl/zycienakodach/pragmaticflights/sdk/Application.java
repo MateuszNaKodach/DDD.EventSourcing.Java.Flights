b41420968cd7024e0bf3ed2c178b3b8dd2dd8d6b
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-10-06 14:33:44 +0200

    Extract ApplicationConfig interface

--- src/main/java/pl/zycienakodach/pragmaticflights/sdk/Application.java
+++ src/main/java/pl/zycienakodach/pragmaticflights/sdk/Application.java
@@ -14,30 +14,12 @@ import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
-public interface Application {
-  <E> Application when(Class<E> eventType, EventHandler<E> handler);
-
-  <E> Application when(Class<E> eventType, Function<E, ?> command);
-
-  <E> Application when(Class<E> eventType, EventHandler<E> handler, EventFilter<E> filter);
-
-  <C, E> Application onCommand(Class<C> commandType, BiFunction<C, CommandMetadata, EventStreamName> streamName, Function<C, DomainLogic<E>> domainLogic);
-
-  <C, E> Application onCommand(Class<C> commandType, BiFunction<C, CommandMetadata, EventStreamName> streamName, BiFunction<C, CommandMetadata, DomainLogic<E>> domainLogic);
-
-  <C> Application onCommand(Class<C> commandType, CommandHandler<C> handler);
+public interface Application extends ApplicationConfig {
 
   <T> Application execute(T command, ApplicationContext context);
 
   <T> CommandResult execute(T command, CommandMetadata metadata);
 
-  Application withModule(ApplicationModule module);
-
-  Application withModules(List<ApplicationModule> module);
-
   <E> void storeEvent(EventStreamName eventStream, E event, EventMetadata metadata);
 
-  String generateId();
-
-  Instant currentTime();
 }
